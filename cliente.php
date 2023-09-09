990633e402ab958a39dfec3c5ca0722053c6889c
Ale241302 <128639865+Ale241302@users.noreply.github.com>
2023-09-08 20:41:20 -0500

    subir

--- cliente.php
+++ cliente.php
@@ -1,77 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Calculadora</title>
+    <title>Hello Word</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container mt-5">
-        <h2 class="mb-4">Calculadora</h2>
+        <h2 class="mb-4">Word</h2>
         <form method="post" action="">
 
         <div class="form-group">
-                <label for="num1">Nombre a Contar Caracteres:</label>
+                <label for="num1">Ingrese Su Nombre:</label>
                 <input type="string" class="form-control" name="nomb" required>
             </div>
-
-            <div class="form-group">
-                <label for="num1">Número 1:</label>
-                <input type="number" class="form-control" name="num1" required>
-            </div>
             
-            <div class="form-group">
-                <label for="num2">Número 2:</label>
-                <input type="number" class="form-control" name="num2" required>
-            </div>
-            
-            <div class="form-group">
-                <label for="operacion">Operación:</label>
-                <select class="form-control" name="operacion">
-                    <option value="suma">Suma</option>
-                    <option value="resta">Resta</option>
-                    <option value="multiplicacion">Multiplicación</option>
-                    <option value="division">División</option>
-                </select>
-            </div>
-            
-            <button type="submit" class="btn btn-primary" name="calcular">Calcular</button>
+            <button type="submit" class="btn btn-primary" name="calcular">Enviar</button>
         </form>
 
        
         <?php
         if(isset($_POST['calcular'])) {
         $nomb = $_POST['nomb'];
-        $num1 = $_POST['num1'];
-        $num2 = $_POST['num2'];
-        $operacion = $_POST['operacion'];
         
         $options = array(
-            'location' => 'http://localhost/tarea1_soap_Casierra/soapserver.php', 
-            'uri' => 'http://localhost/tarea1_soap_Casierra/soapserver.php', 
+            'location' => 'https://servicios.documentosige.com.co/service.php', 
+            'uri' => 'https://servicios.documentosige.com.co/service.php?wsdl', 
         );
-
-        try {
             $client = new SoapClient(null, $options);
-            $resultado2 = $client->conteo($nomb);
-            if($operacion === 'suma') {
-                $resultado = $client->suma($num1, $num2);
-            } elseif($operacion === 'resta') {
-                $resultado = $client->resta($num1, $num2);
-            } elseif($operacion === 'multiplicacion') {
-                $resultado = $client->multiplicacion($num1, $num2);
-            } elseif($operacion === 'division') {
-                if($num2 != 0) {
-                    $resultado = $client->division($num1, $num2);
-                } else {
-                    echo "No se puede dividir por cero.";
-                }
-            }
-
-            echo "Resultado Operación: $resultado<br>";
-            echo "Resultado Conteo Caracteres: $resultado2";            
-        } catch (SoapFault $e) {
-            echo "Error: " . $e->getMessage();
-        } 
+            $resultado2 = $client->hello($nomb);
+            echo "Resultado: $resultado2";            
     }
     ?>
     </div>
